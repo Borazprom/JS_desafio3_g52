461045fe6236007e2d421f89c86e2f3af0b736b7
Borazprom <felipe.borbaran@gmail.com>
2023-11-28 21:16:32 -0300

    Requerimeinto 2.2 listo  y 2.3 sin hacer

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// function pintar() {
+//     ele.style.backgroundColor = "yellow";
+//   }
+
+//   const ele = document.getElementById("ele1");
+//   ele.addEventListener("click", pintar);
+
+
+
+//  REQUERIMIENTO 2.2
+  ele1.addEventListener("click", function() {
+    ele1.style.backgroundColor = "yellow";
+  });
+
+//   NO ENTIENDO EL REQUERIMIENTO 2.3
